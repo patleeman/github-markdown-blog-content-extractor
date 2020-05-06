a13ddda0d10466645a81e9fc57b0a127a11ebcbb
Patrick Lee <me@patricklee.nyc>
2020-05-05 22:29:35 -0400

    Remove try/catch

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ async function getBlogPosts(repo, contentFolder = "") {
   if (!repo) throw new Error("Repo not provided");
   const GITHUB_API = `https://api.github.com/repos/${repo}/contents/${contentFolder}`;
   const blogPosts = [];
-  try {
-    const response = await fetch(GITHUB_API);
-    const data = await response.json();
-    data.map((file) => {
-      blogPosts.push(fetch(file.download_url).then((r) => r.text()));
-    });
-    return await Promise.all(blogPosts);
-  } catch (e) {
-    throw new Error("Error while fetching content");
-  }
+  const response = await fetch(GITHUB_API);
+  const data = await response.json();
+  data.map((file) => {
+    blogPosts.push(fetch(file.download_url).then((r) => r.text()));
+  });
+  return await Promise.all(blogPosts);
 }
 
 module.exports = getBlogPosts;
